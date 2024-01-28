255823fdba2522982d11f19fc86cd44be210248e
Powell Hamner <jacksonhamner@gmail.com>
2024-01-27 16:35:56 -0800

    added Update and Delete to the ReplyService

--- BlazorForums.Services/ReplyServices/IReplyService.cs
+++ BlazorForums.Services/ReplyServices/IReplyService.cs
@@ -5,6 +5,8 @@ namespace BlazorForums.Services.ReplyServices
     public interface IReplyService
     {
         Task<IEnumerable<ReplyClientModel>> GetAsync(int postId);
-        Task<ReplyClientModel> CreateReply(int postId, string userId, string content);
+        Task<ReplyClientModel> Create(int postId, string userId, string content);
+        Task<ReplyClientModel> Update(int replyId, string content);
+        Task<ReplyClientModel> Delete(int replyId);
     }
 }
--- BlazorForums.Services/ReplyServices/ReplyService.cs
+++ BlazorForums.Services/ReplyServices/ReplyService.cs
@@ -22,7 +22,7 @@ namespace BlazorForums.Services.ReplyServices
             return Mapper.Map<IEnumerable<ReplyClientModel>>(replies);
         }
 
-        public async Task<ReplyClientModel> CreateReply(int postId, string userId, string content)
+        public async Task<ReplyClientModel> Create(int postId, string userId, string content)
         {
             var user = await DataContext.Users.SingleOrDefaultAsync(user => user.Id == userId)
                 ?? throw new KeyNotFoundException();
@@ -42,6 +42,27 @@ namespace BlazorForums.Services.ReplyServices
             await DataContext.SaveChangesAsync();
 
             return Mapper.Map<ReplyClientModel>(reply);
+        }
+
+        public async Task<ReplyClientModel> Update(int replyId, string content)
+        {
+            var reply = await DataContext.Replies.SingleOrDefaultAsync(reply => reply.Id == replyId)
+                ?? throw new KeyNotFoundException();
+
+            reply.Content = content;
+
+            this.DataContext.Replies.Update(reply);
+            await this.DataContext.SaveChangesAsync();
+
+            return Mapper.Map<ReplyClientModel>(reply);
+        }
+
+        // To not break the flow of a Post, instead of deleting the reply we will just 
+        // set the message to deleted. 
+        public async Task<ReplyClientModel> Delete(int replyId)
+        {
+            string message = "This message has been deleted.";
+            return await this.Update(replyId, message);
         }
 
     }
